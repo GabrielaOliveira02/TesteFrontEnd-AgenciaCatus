95262cd6561a061764868f8f4653792d4b1e0804
gabizinha <gabrielamiyazaki55@gmail.com>
2024-06-13 12:24:13 -0300

    files outside the header

---
+++ src/scripts/index.js
@@ -0,0 +1,10 @@
+import {productsList} from './productsList.js'
+import { cardList, scrollLeft, scrollRight} from './cards.js'
+import {toggleMenu} from './menu.js'
+
+
+cardList(productsList);
+scrollLeft();
+scrollRight();
+toggleMenu();
+
--- src/scripts/productsList.js
+++ src/scripts/productsList.js
@@ -1,9 +1,58 @@
 export const productsList = [
     {
-        img: "",
-        description: "",
-        price: 582.00,
-        sale: 238.41,
+        img: "./src/assets/product1.jpg",
+        description: "Aliquam congue nisl ele ac ligula convallis, luctus blandit augue.",
+        price: '582,90',
+        sale: '238,41',
+        
+    },
+    {
+        img: "./src/assets/product2.jpg",
+        description: "Aliquam congue nisl ele ac ligula convallis, luctus blandit augue.",
+        price: '582,90',
+        sale: '238,41',
+        
+    },
+    {
+        img: "./src/assets/product3.jpg",
+        description: "Aliquam congue nisl ele ac ligula convallis, luctus blandit augue.",
+        price: '582,90',
+        sale: '238,41',
+        
+    },
+    {
+        img: "./src/assets/product4.jpg",
+        description: "Aliquam congue nisl ele ac ligula convallis, luctus blandit augue.",
+        price: '582,90',
+        sale: '238,41',
+        
+    },
+    {
+        img: "./src/assets/product1.jpg",
+        description: "Aliquam congue nisl ele ac ligula convallis, luctus blandit augue.",
+        price: '582,90',
+        sale: '238,41',
+        
+    },
+    {
+        img: "./src/assets/product2.jpg",
+        description: "Aliquam congue nisl ele ac ligula convallis, luctus blandit augue.",
+        price: '582,90',
+        sale: '238,41',
+        
+    },
+    {
+        img: "./src/assets/product3.jpg",
+        description: "Aliquam congue nisl ele ac ligula convallis, luctus blandit augue.",
+        price: '582,90',
+        sale: '238,41',
+        
+    },
+    {
+        img: "./src/assets/product4.jpg",
+        description: "Aliquam congue nisl ele ac ligula convallis, luctus blandit augue.",
+        price: '582,90',
+        sale: '238,41',
         
     }
 ]
